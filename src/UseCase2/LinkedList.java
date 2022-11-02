f6fb55053de8baad3913a79e27d9d31f578c5d03
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-02 22:52:40 +0530

    Added:uc2 create linkedlist adding by sequence

---
+++ src/UseCase2/LinkedList.java
@@ -0,0 +1,46 @@
+package UseCase2;
+
+public class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head,tail=null ;
+    public void adding(int data) {
+        Node newNode = new Node(data);
+        if(head ==null){
+        // Node newNode;
+        head = newNode;
+       // tail = newNode;
+    }
+        else
+            newNode.next=head;
+        head = newNode;
+        tail = newNode;
+    }
+
+    public void printList() {
+        Node currnode = head;
+        while (currnode != null) {
+            System.out.println(currnode.data + "");
+            currnode = currnode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Linked List Program");
+        LinkedList list = new LinkedList();
+        System.out.println();
+        list.adding(56);
+        list.adding(30);
+        list.adding(70);
+        list.printList();
+
+    }
+}
